f05368cc9e97e43ad6cfa0a3d554572617209983
Shubhkiller <shubhamtripathi033@gmail.com>
2024-01-13 12:41:16 +0530

    changes

--- src/App.js
+++ src/App.js
@@ -3,22 +3,20 @@ import './App.css';
 
 function App() {
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
+<main id="main">
+  <div id="title">Norman Borlaug - The Father of the Green Revolution</div>
+  <div id="img-div">
+    <img id="image" src="https://upload.wikimedia.org/wikipedia/commons/e/e2/Norman_Borlaug%2C_2004_%28cropped%29.jpg" alt="Norman Borlaug" />
+    <div id="img-caption">Borlaug in 2004</div>
+  </div>
+  <div id="tribute-info">
+    <p>Norman Borlaug (1914-2009) was an American agronomist and humanitarian who played a crucial role in the Green Revolution. His work in developing high-yielding varieties of wheat helped alleviate hunger and improve food security around the world.</p>
+    <p>Borlaug's efforts to increase agricultural productivity, combined with his dedication to fighting poverty and promoting sustainable farming practices, earned him the Nobel Peace Prize in 1970. His innovative approaches to agriculture have had a lasting impact on global food production.</p>
+    <p>Throughout his career, Borlaug worked tirelessly to address global food shortages, advocating for the use of scientific advancements to improve crop yields. His legacy continues to inspire individuals and organizations committed to addressing world hunger and promoting sustainable agriculture.</p>
+  </div>
+  <a id="tribute-link" href="https://en.wikipedia.org/wiki/Norman_Borlaug" target="_blank">Learn more about Norman Borlaug</a>
+</main>
+
   );
 }
 
